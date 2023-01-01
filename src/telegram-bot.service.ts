59b3956f7b816ac1461cae142ed2b870646e9c48
Siarhei Pol <siarhei_pol@epam.com>
2023-01-01 16:21:03 +0400

    Updated run script for google cloud. Temporary fixed the issue with telegram-bot.

--- src/telegram-bot.service.ts
+++ src/telegram-bot.service.ts
@@ -12,6 +12,7 @@ export class TelegramBotService {
     private bot: TelegramBot;
     private storageFilePath = `${process.cwd()}/list_chat_id.json`;
     private subscribePassphrase: string = process.env.TELEGRAM_BOT_SUBSCRIBE_PASSPHRASE;
+    private listChats: Array<number> = [];
 
     constructor() {
         const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -27,12 +28,12 @@ export class TelegramBotService {
             const text = msg.text;
             const chatId = msg.chat.id;
 
-            console.log(text);
+            console.log(`INFO: TelegramBot get ${text}`);
 
             // todo стоит ли делать более удобный интерфейс боту
             try {
-                const subscibeCommand = `/subscribe ${this.subscribePassphrase}`;
-                if(text === subscibeCommand) {
+                const subscribeCommand = `/subscribe ${this.subscribePassphrase}`;
+                if(text === subscribeCommand) {
                     this.UpdateChatIdStorage(chatId, ChatIdAction.store);
                     this.bot.sendMessage(chatId, 'You successfully subscribed.');
                 } else if(text === '/unsubsribe') {
@@ -40,7 +41,8 @@ export class TelegramBotService {
                     this.bot.sendMessage(chatId, 'You unsubscribed.');
                 }
             } catch (e) {
-                console.log(`Telegram Bot error in chat ${chatId}`);
+                console.log(`ERROR: Telegram Bot error in chat ${chatId}`);
+				console.log(e);
             }
         })
     }
@@ -60,34 +62,49 @@ export class TelegramBotService {
     }
 
     private UpdateChatIdStorage(chatId: number, action: ChatIdAction){
-        let listChats: Array<number> = [];
+        // let listChats: Array<number> = [];
+        //
+        // if(fs.existsSync( this.storageFilePath )) {
+        //     const contentFile = fs.readFileSync(this.storageFilePath);
+        //     listChats = JSON.parse(contentFile.toString());
+        //
+        //     const found = listChats.findIndex(element => element == chatId);
+        //
+        //     // Adding chartId in the existing file
+        //     if(action == ChatIdAction.store) {
+        //         if(found == -1) {
+        //             listChats.push(chatId);
+        //         }
+        //     }
+        //     // Removing chartId from the existing file
+        //     if(action == ChatIdAction.clear){
+        //         if(found != -1) {
+        //             delete listChats[found];
+        //         }
+        //     }
+        // }
+        // else {
+        //     // Adding chartId in a new file
+        //     if(action == ChatIdAction.store) {
+        //         listChats.push(chatId);
+        //     }
+        // }
+        //
+        // fs.writeFileSync(this.storageFilePath, JSON.stringify(listChats));
 
-        if(fs.existsSync( this.storageFilePath )) {
-            const contentFile = fs.readFileSync(this.storageFilePath);
-            listChats = JSON.parse(contentFile.toString());
-
-            const found = listChats.findIndex(element => element == chatId);
+        const found = this.listChats.findIndex(element => element == chatId);
 
             // Adding chartId in the existing file
             if(action == ChatIdAction.store) {
                 if(found == -1) {
-                    listChats.push(chatId);
+                    this.listChats.push(chatId);
                 }
             }
             // Removing chartId from the existing file
             if(action == ChatIdAction.clear){
                 if(found != -1) {
-                    delete listChats[found];
+                    delete this.listChats[found];
                 }
             }
-        }
-        else {
-            // Adding chartId in a new file
-            if(action == ChatIdAction.store) {
-                listChats.push(chatId);
-            }
-        }
-
-        fs.writeFileSync(this.storageFilePath, JSON.stringify(listChats));
     }
 }
